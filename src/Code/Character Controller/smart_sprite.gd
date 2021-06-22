9f29c05e34897f9053b7e2a155e40188d4107ddb
Yonatan Attali <yonatan.attali@gmail.com>
2021-06-22 17:02:09 -0400

    Added blendspace animations for the player

--- src/Code/Character Controller/smart_sprite.gd
+++ src/Code/Character Controller/smart_sprite.gd
@@ -13,6 +13,7 @@ export(bool) var DEBUG = false
 
 
 onready var kb = get_parent() as KinematicBody2D
+onready var anim_tree = kb.get_node("AnimationTree") as AnimationTree
 
 
 func _process(delta):
@@ -24,13 +25,22 @@ func _process(delta):
 		print("SmartSprite requires 'direction' meta variable")
 		return
 	
-	
 	if kb.get_meta("direction") != Vector2.ZERO:
+		anim_tree["parameters/Idle/blend_position"] = kb.get_meta("direction")
+		anim_tree["parameters/Walk/blend_position"] = kb.get_meta("direction")
 		flip_h = kb.get_meta("direction").x < 0
-		play("Walk")
+		
+		anim_tree["parameters/idle_walk/blend_amount"] = 0
 	else:
-		play("Idle")
-
+		anim_tree["parameters/idle_walk/blend_amount"] = 1
+		pass
+	
+#	if kb.get_meta("direction") != Vector2.ZERO:
+#		flip_h = kb.get_meta("direction").x < 0
+#		play("Walk")
+#	else:
+#		play("Idle")
+	
 	if DEBUG:
 		update() # the draw method
 
