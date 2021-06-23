f8c46c0629b5705592d000bf77701cc76c9333bd
Yonatan Attali <yonatan.attali@gmail.com>
2021-06-22 21:16:26 -0400

    Correct animations now play when the player is idling and walking

--- src/Code/Character Controller/smart_sprite.gd
+++ src/Code/Character Controller/smart_sprite.gd
@@ -16,6 +16,10 @@ onready var kb = get_parent() as KinematicBody2D
 onready var anim_tree = kb.get_node("AnimationTree") as AnimationTree
 
 
+var ang_index
+var suffix = "Right"
+
+
 func _process(delta):
 	if not kb:
 		print("SmartSprite requires a KinematicBody2D as a parent")
@@ -25,21 +29,29 @@ func _process(delta):
 		print("SmartSprite requires 'direction' meta variable")
 		return
 	
+#	if kb.get_meta("direction") != Vector2.ZERO:
+#		anim_tree["parameters/Idle/blend_position"] = kb.get_meta("direction")
+#		anim_tree["parameters/Walk/blend_position"] = kb.get_meta("direction")
+#		flip_h = kb.get_meta("direction").x < 0
+#
+#		anim_tree["parameters/idle_walk/blend_amount"] = 0
+#	else:
+#		anim_tree["parameters/idle_walk/blend_amount"] = 1
+#		pass
+	
+	
 	if kb.get_meta("direction") != Vector2.ZERO:
-		anim_tree["parameters/Idle/blend_position"] = kb.get_meta("direction")
-		anim_tree["parameters/Walk/blend_position"] = kb.get_meta("direction")
 		flip_h = kb.get_meta("direction").x < 0
 		
-		anim_tree["parameters/idle_walk/blend_amount"] = 0
+		var ang = kb.get_meta("rotation") # slightly embarassing code o_o
+		ang_index = stepify( lerp(0, 4, (ang+(1.33*PI)) / (2*PI)), 0.5 )
+		suffix = "Down" if ang_index == 3.5 else "Right"
+		suffix = "Up" if ang_index == 1.5 else suffix
+		
+		play("Walk %s" % [suffix])
 	else:
-		anim_tree["parameters/idle_walk/blend_amount"] = 1
-		pass
+		play("Idle %s" % [suffix])
 	
-#	if kb.get_meta("direction") != Vector2.ZERO:
-#		flip_h = kb.get_meta("direction").x < 0
-#		play("Walk")
-#	else:
-#		play("Idle")
 	
 	if DEBUG:
 		update() # the draw method
@@ -47,11 +59,14 @@ func _process(delta):
 
 var default_font = Control.new().get_font("font") # just get the default font
 func _draw():
-	if DEBUG:
-		draw_string(default_font, Vector2(64, 0), animation)
-
-
-
+	if not DEBUG:
+		return 
+	
+	draw_string(default_font, Vector2(64, 0), animation)
+	
+	draw_line(Vector2(), kb.get_meta("direction").normalized()*30, Color("#ffaaff"), 2.0)
+	
+	draw_string(default_font, Vector2(64, 20), str(ang_index))
 
 
 
