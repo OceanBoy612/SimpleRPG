1e9c32a10fa622910c9578cc28fa57b7414b7272
Yonatan Attali <yonatan.attali@gmail.com>
2021-06-26 15:09:27 -0400

    Update Frim attack pattern
    Frim now jump into the air, move in the direction of the target a fixed amount then detonate dealing damage in an area.

--- src/Code/Enemies/attack.gd
+++ src/Code/Enemies/attack.gd
@@ -5,64 +5,75 @@ attacks once
 ends when animation finishes
 """
 
-var anim : AnimatedSprite
 export var damage_amount = 1
+var anim : AnimatedSprite
+
+
+var dir : Vector2
+var vel : float
+
+var jump_duration = 0.4
 
 func on_enable():
-#	anim = (kb.get_node("AnimatedSprite") as AnimatedSprite)
-#	anim.connect("animation_finished", self, "on_animation_finished")
-#	anim.play("Attack")
-	var player = (kb.get_node("AnimationPlayer") as AnimationPlayer)
-	player.play("Attack")
+	anim = (kb.get_node("AnimatedSprite") as AnimatedSprite)
+	if not anim.is_connected("animation_finished", self, "on_animation_finished"):
+		anim.connect("animation_finished", self, "on_animation_finished")
+	anim.play("Leap")
+	
+#
+	$Tween.interpolate_property(self, "vel", 
+			250, 0, jump_duration, Tween.TRANS_SINE, Tween.EASE_IN, 0.6)
+	$Tween.start()
+	dir = ((kb as CombatEntity).target.global_position - global_position).normalized()
+	
+	# disable collision
+	kb.collision_mask = kb.collision_mask & ~2 # Player layer
+	kb.collision_mask = kb.collision_mask & ~4 # Enemy layer
+	
+	
+#	var player = (kb.get_node("AnimationPlayer") as AnimationPlayer)
+#	player.play("Attack")
 #	player.connect("animation_finished", "on_animation_finished")
 
 
+func _physics_process(delta):
+	kb.move_and_slide(vel * dir)
+	pass
 
 
-func _on_AnimationPlayer_animation_finished(anim_name):
-	(kb as CombatEntity).last_attack_time = 0
-	(kb as CombatEntity).target = null
-	emit_signal("completed")
-	pass # Replace with function body.
 
 
 
 func on_animation_finished():
-	return 
-	anim.disconnect("animation_finished", self, "on_animation_finished")
-	(kb as CombatEntity).last_attack_time = 0
-	(kb as CombatEntity).target = null
-	anim.play("Idle")
+	if anim.animation == "Leap":
+		anim.play("Air")
+	elif anim.animation == "Detonate":
+		anim.disconnect("animation_finished", self, "on_animation_finished")
+		anim.play("Idle")
+		(kb as CombatEntity).last_attack_time = 0
+		(kb as CombatEntity).target = null
+		
+		emit_signal("completed")
 	
-	var area = (kb.get_node("AttackArea") as Area2D)
 	
-	var bodies = area.get_overlapping_bodies()
-	for b in bodies:
-		if b == kb: continue
-		if b.has_method("damage"):
-			b.damage(damage_amount)
-		if b.has_method("knockback"):
-			b.knockback(kb, damage_amount)
+
+
+
+func _on_Tween_tween_all_completed():
+	anim.play("Detonate")
 	
-	emit_signal("completed")
-	pass
-
-### Helpers ###
-
-
-
-### Helpers ###
+	var pl = kb.get_node("AnimationPlayer") as AnimationPlayer
+	pl.play("Attack") # aoe damage area expansion and enable
+	
+	# re-enable collision
+	kb.collision_mask = kb.collision_mask | 2 # Player layer
+	kb.collision_mask = kb.collision_mask | 4 # Enemy layer
 
 
-### DEBUG ###
 
-#func _draw():
-#	if not DEBUG:
-#		return
-#
-#	# TODO: This should move to the CombatEntity debug class
-#	draw_arc(Vector2(), kb.attack_range, 0, 2*PI, 32, Color("#ff1111"))
-#
-#
+
+
+
+
 
 
